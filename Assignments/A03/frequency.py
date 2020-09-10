03be3f360b1af61707eafdec582cea3f49ed6d4e
maepreville <54781760+maepreville@users.noreply.github.com>
2020-09-10 14:42:08 -0500

    Update frequency.py

--- Assignments/A03/frequency.py
+++ Assignments/A03/frequency.py
@@ -9,6 +9,7 @@ class Frequency():
         self.text = ""
         self.freq = {}
         self.sort_freq = None
+        self.total = 0
 
         for l in alphabet:
             self.freq[l] = 0
@@ -18,6 +19,7 @@ class Frequency():
             l = l.lower()
             if l in alphabet:
                 self.freq[l] += 1
+                self.total += 1
 
         # https://realpython.com/python-lambda/
         self.sort_freq = sorted(self.freq.items(), key=lambda x: x[1], reverse=True)
@@ -29,21 +31,37 @@ class Frequency():
         else:
             print(self.freq)
 
+    def print_table(self):
+        for i in range(5):
+            for j in range(5):
+                if j > 0:
+                    print(" , ",end="")
+                pct = (int(self.sort_freq[i*5+j][1])/self.total)*100
+                pct = "{:5.2f}".format(pct)
+                print(f"{self.sort_freq[i*5+j][0]}:{pct}%",end=" ")
+            print("")
+
+
     def getNth(self,n):
         if self.sort_freq:
             return self.sort_freq[n][0]
 
         return None
 
-if __name__=='__main__':
-    url = "https://github.com/maepreville/4663-Cryptography-Preville/blob/master/Assignments/A03/ciphertext_1.txt"
-    #url = "https://github.com/maepreville/4663-Cryptography-Preville/blob/master/Assignments/A03/ciphertext_2.txt"
-    print("Downloading text ...")
-    f = requests.get(url)
-    text = f.text
 
-    print("Calculating frequency...")
-    F = Frequency()
-    F.count(text)
-    F.print()
-    print(F.getNth(2))
+if __name__=='__main__':
+  
+  f = open("ciphertext1.txt","r")
+ 
+  text = f.read()
+  
+  print(" ")  
+  print("Calculating frequency...")
+
+  F = Frequency()
+
+  F.count(text)
+
+  F.print_table()
+
+  print(F.getNth(2))
